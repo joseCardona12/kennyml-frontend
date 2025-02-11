c3ece60bc476f32dcd340a1f6b32295c6c8bfe0f
joseCardona12 <josesiprozmater@gmail.com>
2025-02-11 15:49:18 -0500

    fix: solving problem with view settingEmail

--- src/app/(main)/(private)/dashboard/setting/email/page.tsx
+++ src/app/(main)/(private)/dashboard/setting/email/page.tsx
@@ -1,10 +1,21 @@
+"use client";
 import { SectionEditEmail } from "@/ui/organisms";
 import { DashboardTemplateV2 } from "@/ui/templates";
 
 export default function SettingEmailView() {
   return (
     <DashboardTemplateV2
-      section={<SectionEditEmail />}
+      section={
+        <SectionEditEmail
+          name=""
+          onChange={() => console.log("change")}
+          onClick={() => console.log("click")}
+          placeholder=""
+          textButton=""
+          type=""
+          value=""
+        />
+      }
       title_section="Information about Email"
       url_image="/images/avatar_random.jpg"
     />
